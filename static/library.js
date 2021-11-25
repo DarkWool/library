327ab228bf28f8b7f13644051791e25fbddf35cd
DarkWool <cooper27@live.com.mx>
2021-11-25 00:53:16 -0600

    Included a function to add a book with an HTML form and another one to show the books in a table.

--- static/library.js
+++ static/library.js
@@ -1,3 +1,42 @@
+let library = [
+    {title: 'The Hobbit', author: 'J.R.R. Tolkien', pagesNumber: 295, wasRead: 'not yet read'},
+    {title: 'The Hobbit', author: 'J.R.R. Tolkien', pagesNumber: 295, wasRead: 'not yet read'},
+    {title: 'The Hobbit', author: 'J.R.R. Tolkien', pagesNumber: 295, wasRead: 'not yet read'}
+];
+
+let form = document.getElementsByName('addBook')[0];
+let tBody = document.getElementsByTagName('tbody')[0];
+
+form.addEventListener("submit", addBookToLibrary);
+
+function addBookToLibrary(e) {
+    e.preventDefault();
+    library.push(new Book(form.elements['getBookName'].value,
+        form.elements['getBookAuthor'].value,
+        form.elements['getPagesNumber'].value,
+        form.elements['wasRead'].checked));
+
+    showBooks();
+    form.reset();
+}
+
+function showBooks() {
+    tBody.innerHTML = '';
+    
+    library.map(el => {
+        let row = document.createElement('tr');
+        tBody.append(row);
+        
+        for (key in el) {
+            if (!el.hasOwnProperty(key)) return;
+            
+            let tData = document.createElement('td');
+            tData.textContent = el[key];
+            row.append(tData);
+        }
+    })
+}
+
 function Book(title, author, pagesNumber, wasRead) {
     this.title = title;
     this.author = author;
@@ -11,4 +50,4 @@ Book.prototype = {
     }
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, "not yet read");
+showBooks();
