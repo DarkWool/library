44ab7bb1313cbe04346facb3de51bb66d2c3cf72
DarkWool <cooper27@live.com.mx>
2021-12-05 01:21:29 -0600

    Added functionality for the progress bar and to detect which theme the user prefers followed by general tweaks.

--- static/library.js
+++ static/library.js
@@ -1,20 +1,42 @@
 let library = [
-    { title: 'The Hobbit', author: 'J.R.R. Tolkien', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In facilisis euismod magna, quis rutrum ante. Maecenas aliquet, velit in iaculis rhoncus, est nulla fermentum elit, sed suscipit sapien nisl eu orci. Nullam pharetra felis ac faucibus bibendum. Quisque at nisl augue. Cras commodo semper ipsum sed efficitur. Sed non feugiat sem. Fusce rutrum at libero vitae dignissim. Suspendisse dictum a tortor eget convallis. Nam et sem nec augue venenatis dignissim. Vestibulum mi nulla, venenatis vitae placerat a', pagesNumber: 295, wasRead: false},
-    { title: 'The Hobbit', author: 'J.R.R. Tolkien', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In facilisis euismod magna, quis rutrum ante. Maecenas aliquet, velit in iaculis rhoncus, est nulla fermentum elit, sed suscipit sapien nisl eu orci. Nullam pharetra felis ac faucibus bibendum. Quisque at nisl augue. Cras commodo semper ipsum sed efficitur. Sed non feugiat sem. Fusce rutrum at libero vitae dignissim. Suspendisse dictum a tortor eget convallis. Nam et sem nec augue venenatis dignissim. Vestibulum mi nulla, venenatis vitae placerat a', pagesNumber: 295, wasRead: true},
-    {title: 'The Hobbit', author: 'J.R.R. Tolkien', pagesNumber: 295, wasRead: false},
-    {title: 'Huesos de Lagartija.', author: 'Federico Navarrete', pagesNumber: 410, wasRead: false}
+    {
+        title: 'Harry Potter and the Order of the Phoenix',
+        author: 'J. K. Rowling',
+        description: `Harry Potter is in his fifth year at Hogwarts School as the adventures continue. There is a door at the end of a silent corridor. And it's haunting Harry Potter's dreams. Why else would he be waking in the middle of the night, screaming in terror? Harry has a lot on his mind for this, his fifth year at Hogwarts: a Defence Against the Dark Arts teacher with a personality like poisoned honey; a big surprise on the Gryffindor Quidditch team; and the looming terror of the Ordinary Wizarding Level exams.`,
+        pagesNumber: 766,
+        wasRead: true
+    },
+    {
+        title: 'Letters from a Stoic',
+        author: 'Lucius Annaeus Seneca',
+        description: `For several years of his turbulent life, in which he was dogged by ill health, exile and danger, Seneca was the guiding hand of the Roman Empire. This selection of Seneca's letters shows him upholding the ideals of Stoicism - the wisdom of the self-possessed person immune to life's setbacks - while valuing friendship and courage, and criticizing the harsh treatment of slaves and the cruelties in the gladiatorial arena. A moving and inspiring declaration of the dignity of the individual mind.`,
+        pagesNumber: 254,
+        wasRead: false
+    },
+    {
+        title: 'I Am a Cat',
+        author: 'Natsume Sōseki',
+        pagesNumber: 480,
+        wasRead: false
+    },
+    {
+        title: 'The Hobbit',
+        author: 'J.R.R. Tolkien',
+        description: `Bilbo Baggins is a hobbit who enjoys a comfortable, unambitious life, rarely traveling any farther than his pantry or cellar. But his contentment is disturbed when the wizard Gandalf and a company of dwarves arrive on his doorstep one day to whisk him away on an adventure. They have launched a plot to raid the treasure hoard guarded by Smaug the Magnificent, a large and very dangerous dragon. Bilbo reluctantly joins their quest, unaware that on his journey to the Lonely Mountain he will encounter Gollum.`,
+        pagesNumber: 300,
+        wasRead: true
+    }
 ];
 
 const form = document.getElementsByName('addBookForm')[0];
-const bookLibrary = document.getElementById('libraryContent');
-
-const addCardBtn = document.createElement('div');
-addCardBtn.id = 'addNew';
-addCardBtn.classList.add('card');
-addCardBtn.insertAdjacentHTML('beforeend',
+const bookLibrary = document.getElementById('libraryBody');
+const darkModeBtn = document.getElementById('dark-mode');
+const booksProgress = document.getElementById('books-read');
+const newBookBtn = document.createElement('div');
+newBookBtn.id = 'addNew';
+newBookBtn.classList.add('card');
+newBookBtn.insertAdjacentHTML('beforeend',
 '<span class="material-icons">add_circle</span>');
-
-const darkMode = document.getElementById('dark-mode');
 
 const modal = document.getElementsByClassName('modal')[0];
 const openModalBtn = document.getElementById('addNew');
@@ -22,20 +44,10 @@ const closeModalBtn = document.getElementById('close-button');
 const darkOverlay = document.getElementById('darkOverlay');
 
 form.addEventListener('submit', addBookToLibrary);
-addCardBtn.addEventListener('click', openModal);
+darkModeBtn.addEventListener('click', toggleDarkMode);
+newBookBtn.addEventListener('click', openModal);
 closeModalBtn.addEventListener('click', closeModal)
 darkOverlay.addEventListener('click', closeModal);
-darkMode.addEventListener('click', toggleDarkMode);
-
-function openModal() {
-    darkOverlay.classList.add('active');
-    modal.classList.add('active');
-}
-
-function closeModal() {
-    darkOverlay.removeAttribute('class');
-    modal.classList.remove('active');
-};
 
 function addBookToLibrary(e) {
     // Prevents the form refreshing the page.
@@ -100,14 +112,15 @@ function updateLibrary() {
 
         bookLibrary.append(newCard);
     })
-
+    
     const descButtons = document.getElementsByClassName('desc-btn');
     const readButtons = document.getElementsByClassName('read-btn');
     const deleteButtons = document.getElementsByClassName('delete-btn');
-    bookLibrary.append(addCardBtn);
+    bookLibrary.append(newBookBtn);
+    updateProgressBar();
 
     for (let button of readButtons) {
-        button.addEventListener('click', checkIfRead);
+        button.addEventListener('click', toggleReadStatus);
     }
     
     for (let button of deleteButtons) {
@@ -119,15 +132,30 @@ function updateLibrary() {
     }
 }
 
-function checkIfRead(e) {
+function updateProgressBar() {
+    const totalBooks = library.length;
+
+    if (!totalBooks) {
+        booksProgress.value = 0;
+        return booksProgress.labels[0].textContent = `No books.`;
+    }
+
+    const booksRead = library.reduce((total, el) => (el.wasRead) ? total += 1 : total, 0);
+    booksProgress.value = booksRead / totalBooks * 100;
+    booksProgress.labels[0].textContent = `${booksRead} of ${totalBooks} books read`;
+}
+
+function toggleReadStatus(e) {
     const index = e.target.closest('.card').dataset.index;
     if (library[index].wasRead) {
         e.target.innerHTML = `<i class="material-icons">cancel</i> NOT READ`;
-        return library[index].wasRead = false;
+        library[index].wasRead = false;
+    } else {
+        e.target.innerHTML = `<i class="material-icons">check_circle</i> READ`;
+        library[index].wasRead = true;
     }
     
-    e.target.innerHTML = `<i class="material-icons">check_circle</i> READ`;
-    return library[index].wasRead = true;
+    return updateProgressBar();
 }
 
 function deleteBook(e) {
@@ -135,7 +163,7 @@ function deleteBook(e) {
     library.splice(card.dataset.index, 1);
     card.remove();
     
-    updateLibrary();
+    return updateLibrary();
 }
 
 function showDescription(e) {
@@ -159,8 +187,38 @@ function Book(title, author, description, pagesNumber, wasRead) {
     this.wasRead = wasRead;
 }
 
+// Dark mode functions
 function toggleDarkMode(e) {
-    return (document.body.classList.contains('dark')) ? document.body.classList.remove('dark') : document.body.classList.add('dark');
+    if (document.body.classList.contains('dark')) {
+        document.body.classList.remove('dark');
+        e.target.textContent = 'dark_mode';
+    } else {
+        document.body.classList.add('dark');
+        e.target.textContent = 'light_mode';
+    }
 }
+
+const query = window.matchMedia('(prefers-color-scheme: dark)');
+function setInitialTheme(e) {
+    if (e.matches) {
+        document.body.classList.add('dark');
+        darkModeBtn.textContent = 'light_mode';
+    }
+}
+
+query.addListener(setInitialTheme);
+setInitialTheme(query);
+
+// Modal functions
+function openModal() {
+    darkOverlay.classList.add('active');
+    modal.classList.add('active');
+}
+
+function closeModal() {
+    darkOverlay.removeAttribute('class');
+    modal.classList.remove('active');
+};
+
 
 updateLibrary();
